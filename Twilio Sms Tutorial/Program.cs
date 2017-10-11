69ffa6e7e013b97691959cf80476afea4c590e3b
Muhammad Ansab Iqbal <ansab.iqbal@ba.com>
2017-10-11 10:31:56 +0500

    Initial

---
+++ Twilio Sms Tutorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Twilio_Sms_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your Account SID from twilio.com/console
+            var accountSid = "AC66c6ae920b1c0db9acfe2cbf945f607a";
+            // Your Auth Token from twilio.com/console
+            var authToken = "64434993502cc71a95f188516ffc544f";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                to: new PhoneNumber("+923458638040"),
+                from: new PhoneNumber("+1 208-314-3389 "),
+                body: "Hello from C#");
+
+            Console.WriteLine(message.Sid);
+            Console.Write("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
